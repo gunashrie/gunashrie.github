7214abaa80ef5a46b1abb0ec93108a3ea9f8a061
gunashrie <52064014+gunashrie@users.noreply.github.com>
2020-12-01 20:43:47 +0530

    Add files via upload

---
+++ cabfro.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>mtt2</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAVmjVwUioSv7Tza4fes47yu_RbiA74JGg=initAutocomplete&libraries=places&v=weekly" defer ></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  <div class="elfsight-app-b5af65b6-0e35-4a9d-a5fa-09529a9c69d6"></div>
+  <style type="text/css">
+    body{
+     background-color: #20c997;
+
+     opacity: 10;
+     filter: alpha(opacity=20); /* For IE8 and earlier */
+     background-size:cover;
+     background-blend-mode:darken;
+   }
+   div.transbox{
+    background-image: url("https://img.freepik.com/free-photo/young-businessman-getting-taxi_1098-15507.jpg?size=626&ext=jpg&ga=GA1.2.227712763.1594050891");
+    margin:100px;
+    padding: 20px;
+
+    background-size: cover;
+    color: black;
+    opacity: 0.7;
+    font-weight: bold;
+  }
+  p{
+    margin:100px;
+    padding: 30px;
+    background-size: cover;
+    color: black;
+    background-color:powder blue;
+    border: 0px solid black;
+    opacity: 10;
+  }
+  div.section{
+    height: 100px;
+    margin:200px;
+    padding: 10px;
+    background-size: repeat;
+    color: black;
+    background-color:white;
+    border: 2px solid black;
+    opacity: 0.7;
+  }
+  .carousel-item
+  {
+    position: relative;
+    width: 100%;
+    margin: 90;
+  }
+  .carousal-item img{
+    width: 100%;
+  }
+  .txt{
+    position: absolute;
+    top: 30%;
+    width: 80%;
+    text-align: center;
+    color: blue;
+    font-size: 30px;
+  }
+  div.container{
+    margin:100px;
+    padding: 20px;
+    background-size: cover;
+    color: black;
+    background-color:white;
+    border: 0px solid black;
+    opacity: 0.7;
+  }
+  .phone{
+    padding-right: 20px;
+  }
+  #contact_ul{
+    list-style: none;
+    -webkit-padding-start: 0px;
+  }
+
+
+  #li_email ion-icon{
+    font-size: 1.0em;
+    vertical-align: middle;
+    margin-right: 20px;
+    padding-top: 20px;
+  }
+/*.splitlleft {
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+
+/* Control the left side */
+/*?.left {
+  left: 0;
+  
+}
+
+/* Control the right side */
+/*.right {
+  right: 0;
+
+</style>
+
+</head>
+<body>
+  <nav class="navbar navbar-expand-md navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active margin-right">  
+          <i class="fa fa-car"></i> 
+          <i class="fa fa-car" style="font-size:25px;">MTT</i>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="carousel">
+      <i class="fa fa-car"></i>
+      <i class="fa fa-car" style="font-size:25px;">MTT</i>
+      <i class="fa fa-phone fa-2x" style="font-size: 5px padding:30px; margin: 8px;">8077750432</i>
+      <i class="fa fa-envelope" style="font-size: 25px">mtt.net@gmail.com</i>
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner"  class="carousel slide" data-ride="carousel">
+          <div class="carousel-item active"  data-interval="1000">
+            <img src="https://www.driveu.in/static/web/v4/images/banner02.jpg" class="d-block w-100" alt="...">
+            <p class="txt">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div class="carousel-item" data-interval="1000">
+            <img src="https://www.enterprise.com/content/dam/ent-brand/GettyImages-453470317_1920x1080.jpg" class="d-block w-100" alt="...">
+            <div class="container p-3 my-3 bg-dark text-white">
+              <div class="column">
+                <p class="txt">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+            <div class="carousel-item">
+              <img src="https://www.freegreatpicture.com/files/photo102/50840-car-wallpaper.jpg" class="d-block w-100" alt="...">
+              <p class="txt">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="transbox">
+      <h1 style="text-align: center; font-size: 30px"> <B>BOOKING DETAILS</B> </h1>
+      <form action="cabval.php" method="post">
+        <tabe>
+          <tr style="text-align: center;">
+            <span class="text-danger">*</span>user Name <input type="text" name="name"  style="padding: 3px; width: 300px;margin: 10px;" required><br>
+
+          </tr>
+          <tr>
+            <span class="text-danger">*</span>Phone number <input type="text" name="phone" style="padding: 3px; width: 300px;margin: 10px;" required><br>
+          </tr>
+          <tr>
+           <span class="text-danger">*</span> Travelling date <input type="date" name="datee" style="padding: 5px; width: 300px;margin: 10px;" required><br>
+         </tr>
+         <tr>
+          <span class="text-danger">*</span><span>trip selection</span>
+          <select style="padding: 3px; margin: 10px; width: 300px;" name="trip" required>
+            <option>select</option>
+            <option>Round trip</option>
+            <option>Oneway</option>
+            <option>Perday package</option>
+          </select><br>
+        </tr>
+        <tr>
+          From <input type="text" id="search_term" name="fromloc" style="padding: 3px; margin: 10px; width: 300px;" required><br>
+          To <input type="text" name="toloc" style="padding: 3px; margin: 10px; width: 300px;" required><br>
+          pickup time<input type="time" name="pickuptime" required><b><br>
+            <span class="text-danger">*</span> start date<input type="date" name="start" style="padding: 3px; margin: 10px; width: 300px;"><br>
+            <span class="text-danger">*</span>end date<input type="date" name="endd"style="padding: 3px; margin: 10px; width: 300px;"><br>
+          </tr>
+          <span class="text-danger">*</span> <tr>Seats <select name="seats"style="padding: 3px; margin: 10px; width: 300px;" required>
+            <option>select</option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+            <option>11</option>
+            <option>12</option>
+            <option>13</option>
+            <option>14</option>
+            <option>15</option>
+            <option>16</option>
+            <option>17</option>
+            <option>18</option>
+            <option>19</option>
+          </select><br></tr>
+          <span class="text-danger">*</span> A/C or Non A/C <select value="" name="ac" style="padding: 3px; margin: 10px; width: 200px;" required>
+            <option>select</option>
+            <option>A/C</option>
+            <option>Non A/C</option>
+          </select><br>
+          <script type="text/javascript">
+            function activatePlacesSearch() {
+              var input = document.getElementById('search_term')
+              var autocomplete = new google.maps.places.Autocomplete(input)
+            }
+          </script>
+          <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCfG_sphXlUmm-YXh7PohMAWl15rbR5APg&libraries=places&callback=activatePlacesSearch"></script>
+          <button type="submit" value="s" style=" top:50%; left:50%; margin: 30px;">Continue booking
+          </table>
+        </form> 
+        <src= https://maps.googleapis.com/maps/api/js?key=AIzaSyCfG_sphXlUmm-YXh7PohMAWl15rbR5APg&libraries=placeS>
+      </div> 
+    </div>
+  </body>
+  </html>
